921982aba1f4661589a8530a3184a56647a9bfce
Magdalena Cisowska <magdalena.cisowska@intel.com>
2026-04-13 08:32:04 +0200

    FW: add checking of `core_id != -1` for test requiring SMT
    Before we start looking for adjacent cpus with common `core_id`,
    let's make sure that it is defined. In theory, it can be -1.
    
    Signed-off-by: Magdalena Cisowska <magdalena.cisowska@intel.com>

--- framework/device/cpu/cpu_device.cpp
+++ framework/device/cpu/cpu_device.cpp
@@ -57,6 +57,14 @@ void dump_device_info()
 
 TestResult prepare_test_for_device(struct test *test)
 {
+    auto has_core_id_defined = []() -> bool {
+        for(int idx = 0; idx < thread_count(); idx++) {
+            if (device_info[idx].core_id == -1)
+                return false;
+        }
+        return true;
+    };
+
     auto has_smt = []() -> bool {
         for(int idx = 0; idx < thread_count() - 1; idx++) {
             if (device_info[idx].package_id == device_info[idx + 1].package_id &&
@@ -66,9 +74,15 @@ TestResult prepare_test_for_device(struct test *test)
         return false;
     };
 
-    if (test->flags & test_requires_smt && !has_smt()) {
-        log_skip(CpuTopologyIssueSkipCategory, "Test requires SMT (hyperthreading)");
-        return TestResult::Skipped;
+    if (test->flags & test_requires_smt) {
+        if (!has_core_id_defined()) {
+            log_skip(CpuTopologyIssueSkipCategory, "Test requires topology information");
+            return TestResult::Skipped;
+        }
+        if (!has_smt()) {
+            log_skip(CpuTopologyIssueSkipCategory, "Test requires SMT (hyperthreading)");
+            return TestResult::Skipped;
+        }
     }
     return TestResult::Passed;
 }
